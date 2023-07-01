8107d62a2c1101921ea5c43e57a72627d7391432
jprecht <jospre@hotmail.com>
2023-07-01 13:12:57 -0600

    Create winget.cmd

---
+++ winget.cmd
@@ -0,0 +1,47 @@
+@echo off  
+Echo Install Powertoys and Terminal  
+REM Powertoys  
+winget install Microsoft.Powertoys  
+if %ERRORLEVEL% EQU 0 Echo Powertoys installed successfully.  
+REM Terminal  
+winget install Microsoft.WindowsTerminal  
+if %ERRORLEVEL% EQU 0 Echo Terminal installed successfully.   %ERRORLEVEL%
+Echo Install MSI Afterburner
+winget install Guru3D.Afterburner
+if %ERRORLEVEL% EQU 0 Echo MSI Afterburner installed successfully.   %ERRORLEVEL%
+Echo Install Audacity
+winget install Audacity.Audacity
+if %ERRORLEVEL% EQU 0 Echo Audacity installed successfully.   %ERRORLEVEL%
+Echo Install Discord
+winget install Discord.Discord
+if %ERRORLEVEL% EQU 0 Echo Discord installed successfully.   %ERRORLEVEL%
+Echo Install FileZilla
+winget install FileZilla Client
+if %ERRORLEVEL% EQU 0 Echo FileZilla installed successfully.   %ERRORLEVEL%
+Echo Install Chrome
+winget install Google.Chrome
+if %ERRORLEVEL% EQU 0 Echo Chrome installed successfully.   %ERRORLEVEL%
+Echo Install Skype
+winget install Microsoft.SkypeApp
+if %ERRORLEVEL% EQU 0 Echo Skype installed successfully.   %ERRORLEVEL%
+Echo Install Firefox
+winget install Mozilla.Firefox
+if %ERRORLEVEL% EQU 0 Echo Firefox installed successfully.   %ERRORLEVEL%
+Echo Install Notepad++
+winget install Notepad++.Notepad++
+if %ERRORLEVEL% EQU 0 Echo Notepad++ installed successfully.   %ERRORLEVEL%
+Echo Install Steam
+winget install  Valve.Steam
+if %ERRORLEVEL% EQU 0 Echo Steam installed successfully.   %ERRORLEVEL%
+Echo Install TradingView
+winget install TradingView.TradingViewDesktop
+if %ERRORLEVEL% EQU 0 Echo TradingView installed successfully.   %ERRORLEVEL%
+Echo Install HeavenBenchmark
+winget install Unigine.HeavenBenchmark
+if %ERRORLEVEL% EQU 0 Echo HeavenBenchmark installed successfully.   %ERRORLEVEL%
+Echo Install WinRar
+winget install RARLab.WinRAR
+if %ERRORLEVEL% EQU 0 Echo WinRAR installed successfully.   %ERRORLEVEL%
+Echo Install MyHarmony
+winget install Logitech.MyHarmony
+if %ERRORLEVEL% EQU 0 Echo MyHarmony installed successfully.   %ERRORLEVEL%
